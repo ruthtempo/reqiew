225b78f6146022e888da99956643f3ca283de9b1
Ruth Temporal <ruthtemp@gmail.com>
2023-02-01 16:00:46 +0100

    results display with sorting functionality

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,27 +1,40 @@
 import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  TriangleDownIcon,
+  TriangleUpIcon,
+} from "@chakra-ui/icons";
+
+import {
   Card,
+  Icon,
   Progress,
   Table,
   TableContainer,
+  Tag,
   Tbody,
   Td,
   Th,
   Thead,
   Tr,
+  Text,
+  Flex,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 interface Aspect {
   aspect: string;
   score: number;
+  benchmark: number;
 }
 
 const sentimentRating: Aspect[] = [
-  { aspect: "Quality", score: 90 },
-  { aspect: "Design", score: 93 },
-  { aspect: "Cleaning", score: 61 },
-  { aspect: "Safety", score: 72 },
-  { aspect: "Service", score: 64 },
-  { aspect: "Food Taste", score: 87 },
+  { aspect: "Quality", score: 90, benchmark: 85 },
+  { aspect: "Design", score: 93, benchmark: 87 },
+  { aspect: "Cleaning", score: 61, benchmark: 55 },
+  { aspect: "Safety", score: 72, benchmark: 68 },
+  { aspect: "Service", score: 64, benchmark: 70 },
+  { aspect: "Food Taste", score: 87, benchmark: 90 },
 ];
 
 const assignProgressColor = (score: number) => {
@@ -36,7 +49,26 @@ const assignProgressColor = (score: number) => {
   }
 };
 
+const calculateScoreDiff = (score: number, benchmark: number) => {
+  const result = ((score - benchmark) / benchmark) * 100;
+  return result.toFixed(1);
+};
+
+type Sorted = "ascending" | "descending";
+
 export const Results = () => {
+  const [sortedBy, setSortedBy] = useState<Sorted>("descending");
+
+  const handleSortScores = () => {
+    if (sortedBy === "descending") {
+      sentimentRating.sort((a, b) => a.score - b.score); // lower to higher
+      setSortedBy("ascending");
+    } else {
+      sentimentRating.sort((a, b) => b.score - a.score); //higher to lower
+      setSortedBy("descending");
+    }
+  };
+
   return (
     <Card>
       <TableContainer>
@@ -44,7 +76,17 @@ export const Results = () => {
           <Thead>
             <Tr>
               <Th>Sentiment</Th>
-              <Th>Score</Th>
+              <Th cursor={"pointer"}>
+                Score
+                <Icon
+                  as={
+                    sortedBy === "descending"
+                      ? TriangleDownIcon
+                      : TriangleUpIcon
+                  }
+                  onClick={handleSortScores}
+                />
+              </Th>
               <Th>Benchmark</Th>
             </Tr>
           </Thead>
@@ -60,7 +102,21 @@ export const Results = () => {
                   />
                   {element.score} %
                 </Td>
-                <Td> up or down + % </Td>
+                <Td>
+                  <Flex alignItems={"center"} gap={2}>
+                    <Tag colorScheme={assignProgressColor(element.benchmark)}>
+                      <Icon
+                        as={
+                          element.benchmark < element.score
+                            ? ArrowUpIcon
+                            : ArrowDownIcon
+                        }
+                      />
+                      {calculateScoreDiff(element.score, element.benchmark)} %
+                    </Tag>
+                    <Text fontSize="xs">{element.benchmark} %</Text>
+                  </Flex>
+                </Td>
               </Tr>
             ))}
           </Tbody>
