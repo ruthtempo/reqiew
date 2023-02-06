218d4a4f6fd3e88d31889bd7c4df8dea7e026240
Ruth Temporal <ruthtemp@gmail.com>
2023-02-06 13:13:58 +0100

    adds props to reviews card to make more reusable

--- src/components/ReviewsCard.tsx
+++ src/components/ReviewsCard.tsx
@@ -12,7 +12,14 @@ import { MdStarRate } from "react-icons/md";
 import { Entity } from "../_DATA";
 import { RatingBar } from "./RatingBar";
 
-export const ReviewsCard = (p: { productData: Entity[] }) => {
+export const ReviewsCard = (p: {
+  productData: Entity[];
+  title: string;
+  subtitle: string;
+  barColor: string;
+  starColor: string;
+  tagBg: string;
+}) => {
   const totalRatingProStar = p.productData
     .map((data) => data.rating)
     .reduce(
@@ -34,26 +41,26 @@ export const ReviewsCard = (p: { productData: Entity[] }) => {
     (acc, curr, i) => acc + (5 - i) * (curr / totalReviewSum),
     0
   );
-  console.log(starAverage);
 
   return (
     <Card bg={"gray.100"} variant="filled" borderRadius="2xl">
       <CardHeader>
-        <Heading size="md">Reliable Reviews</Heading>
-        <Text pt="2">Anti-spam filtered reviews</Text>
+        <Heading size="md">{p.title}</Heading>
+        <Text pt="2">{p.subtitle}</Text>
       </CardHeader>
       <CardBody>
         <Flex justifyContent="space-around" gap={4}>
           <Tag variant="outline" borderRadius="full" alignItems="center">
-            <Icon as={MdStarRate} boxSize={15} color="teal" mr={2} />
+            <Icon as={MdStarRate} boxSize={15} color={p.starColor} mr={2} />
             {starAverage ? starAverage.toFixed(1) : 0}/5
           </Tag>
-          <Tag variant="solid" borderRadius="full" bg="teal">
+          <Tag variant="solid" borderRadius="full" bg={p.tagBg}>
             <Text>Total Reviews {totalReviewSum}</Text>
           </Tag>
         </Flex>
         {totalRatingProStar.map((rating, i) => (
           <RatingBar
+            colorScheme={p.barColor}
             star={5 - i}
             reviews={rating}
             value={rating}
